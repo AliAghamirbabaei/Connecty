0c49687fb05b8e7d7993b053f648aefd92bfe2d4
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-15 01:26:09 +0430

    Initial Commit

---
+++ Connecty/ConnectyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectyApp.swift
+//  Connecty
+//
+//  Created by Ali Aghamirbabaei on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ConnectyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
