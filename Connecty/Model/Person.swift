ee3b149039494337821363c7254e85291ccab5f7
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-16 23:38:50 +0430

    Created model for video call

---
+++ Connecty/Model/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  Connecty
+//
+//  Created by Ali Aghamirbabaei on 4/15/22.
+//
+
+import SwiftUI
+
+struct Person {
+    var id: Int
+    var name: String
+    var profilePicture: String
+    var isSpeaking: Bool
+}
