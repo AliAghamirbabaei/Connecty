02c4d10aaed815fa1967a084fe39a39a1e37cb00
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-17 06:08:39 +0430

    Change main view

--- Connecty/ConnectyApp.swift
+++ Connecty/ConnectyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ConnectyApp: App {
     var body: some Scene {
         WindowGroup {
-            VCMembers()
+            VideoCallView()
                 .environmentObject(VideoCallViewModel())
         }
     }
