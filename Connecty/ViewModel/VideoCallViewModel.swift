818c075e147744b461afe66a7bd39bcbc72ce86e
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-17 06:12:01 +0430

    Finish up mocked data

--- Connecty/ViewModel/VideoCallViewModel.swift
+++ Connecty/ViewModel/VideoCallViewModel.swift
@@ -8,42 +8,74 @@
 import SwiftUI
 
 class VideoCallViewModel: ObservableObject {
-    @Published public var people = [[Person]]()
-    public var countOfMembersInCall = 1
+    @Published public var people: [[Person]] = [[], []]
+    public var countOfMembersInCall = 0
+    private var isSecondSectionOpen: Bool = false
+    private var speakTimer: Timer?
     
     init() {
-        addMember()
+        speakTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(makeEveryoneSpeak), userInfo: nil, repeats: true)
     }
     
-    private func addMember() {
-        var allMembers: [Person] = []
-        
-        //countOfMembersInCall += 1
+    public func addMember() {
+        countOfMembersInCall += 1
         
         let names = ["Nathan", "George", "Brian", "Carl", "Tony", "Edward", "Roger", "Steve", "Paul", "Jack"]
-        
-        if countOfMembersInCall > 0 || countOfMembersInCall <= 3 {
-            for id in (1...5).randomElements(countOfMembersInCall) {
-                allMembers.append(Person(id: id, name: names[id], profilePicture: names[id], isSpeaking: false))
+        var randomID: Int?
+        if countOfMembersInCall != 0 {
+            repeat {
+                randomID = (0...9).randomElement()
+            } while people[0].contains(where: {$0.id == randomID}) || people[1].contains(where: {$0.id == randomID})
+            
+            if people[0].count < 3 {
+                withAnimation {
+                    people[0].append(Person(id: randomID!, name: names[randomID!], profilePicture: names[randomID!], isSpeaking: false))
+                }
+            } else {
+                isSecondSectionOpen = true
             }
-            people.append(allMembers)
-        }
-        
-        if countOfMembersInCall >= 4 {
-            allMembers.removeAll()
-            for id in (6...9).randomElements(countOfMembersInCall - 3) {
-                allMembers.append(Person(id: id, name: names[id], profilePicture: names[id], isSpeaking: false))
+            
+            if isSecondSectionOpen {
+                withAnimation {
+                    people[1].append(Person(id: randomID!, name: names[randomID!], profilePicture: names[randomID!], isSpeaking: false))
+                }
             }
-            people.append(allMembers)
         }
-        
-        print("People group 1: \(people[0].count)")
-        print("People group 2: \(people[1].count)")
-        
-        people.shuffle()
     }
     
-    func handleUI() {
+    @objc private func makeEveryoneSpeak() {
+        if !people[0].isEmpty {
+            let canITalk = (0...1).randomElement()!.boolValue
+            
+            if canITalk {
+                let randomMemberID = (0...people[0].count - 1).randomElement()!
+                
+                withAnimation {
+                    people[0][randomMemberID].isSpeaking = true
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.people[0][randomMemberID].isSpeaking = false
+                    }
+                }
+            }
+        }
         
+        if !people[1].isEmpty {
+            let canITalk = (0...1).randomElement()!.boolValue
+            
+            if canITalk {
+                let randomMemberID = (0...people[1].count - 1).randomElement()!
+                
+                withAnimation {
+                    people[1][randomMemberID].isSpeaking = true
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        self.people[1][randomMemberID].isSpeaking = false
+                    }
+                }
+            }
+        }
     }
 }
