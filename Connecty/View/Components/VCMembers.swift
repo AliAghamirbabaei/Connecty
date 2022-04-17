958cd9aa4d1646da6da9a607b71995035ea708c6
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-04-17 06:24:40 +0430

    Added empty state

--- Connecty/View/Components/VCMembers.swift
+++ Connecty/View/Components/VCMembers.swift
@@ -13,31 +13,43 @@ struct VCMembers: View {
     
     var body: some View {
         VStack {
-            if viewModel.countOfMembersInCall != 0 {
-                HStack {
-                    ForEach(viewModel.people[0], id: \.id) { people in
-                        VCPerson(user: people)
-                            .opacity(userOpacity)
-                            .onAppear {
-                                withAnimation(.easeInOut(duration: 1)) {
-                                    userOpacity = 1
+            if viewModel.countOfMembersInCall > 0 {
+                if viewModel.countOfMembersInCall != 0 {
+                    HStack {
+                        ForEach(viewModel.people[0], id: \.id) { people in
+                            VCPerson(user: people)
+                                .opacity(userOpacity)
+                                .onAppear {
+                                    withAnimation(.easeInOut(duration: 1)) {
+                                        userOpacity = 1
+                                    }
                                 }
-                            }
+                        }
                     }
                 }
-            }
-            
-            if viewModel.countOfMembersInCall >= 4 {
-                HStack {
-                    ForEach(viewModel.people[1], id: \.id) { people in
-                        VCPerson(user: people)
-                            .opacity(userOpacity)
-                            .onAppear {
-                                withAnimation(.easeInOut(duration: 1)) {
-                                    userOpacity = 1
+                
+                if viewModel.countOfMembersInCall >= 4 {
+                    HStack {
+                        ForEach(viewModel.people[1], id: \.id) { people in
+                            VCPerson(user: people)
+                                .opacity(userOpacity)
+                                .onAppear {
+                                    withAnimation(.easeInOut(duration: 1)) {
+                                        userOpacity = 1
+                                    }
                                 }
-                            }
+                        }
                     }
+                }
+            } else {
+                HStack(spacing: 10) {
+                    Text("Only you are in call. Please add a person by pressing")
+                        .foregroundColor(.white)
+                        .font(.custom("Quicksand-Regular", size: 16))
+                    
+                    Image("Add-User")
+                        .resizable()
+                        .frame(width: 30, height: 28)
                 }
             }
         }
@@ -46,7 +58,9 @@ struct VCMembers: View {
 
 struct VCMembers_Previews: PreviewProvider {
     static var previews: some View {
-        VStack {
+        ZStack {
+            Color("BG")
+            
             VCMembers()
                 .environmentObject(VideoCallViewModel())
         }
